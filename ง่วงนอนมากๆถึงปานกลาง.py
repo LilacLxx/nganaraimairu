85cc727a87e6b485bcf2f1e1ebb6406ab18c7fb0
LilacLxx <137516733+LilacLxx@users.noreply.github.com>
2024-01-24 15:52:38 +0700

    Add files via upload

---
+++ ง่วงนอนมากๆถึงปานกลาง.py
@@ -0,0 +1,16 @@
+for i in range(1,16,2):
+    print(i, end=',')
+print()
+
+for j in range(2,18,3):
+    print(j, end=',')
+print()
+
+for k in range(30,-31,-10):
+    print(k, end=',')
+print()
+
+for l in range(15,56,8):
+    print(l, end=',')
+print()
+
